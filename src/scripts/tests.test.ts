48e1a42ea81f4bf49af8d7cec46ef1ef6a068113
Michael White <michaelwhite404@gmail.com>
2022-11-30 18:38:51 -0500

    Initialize tests

---
+++ src/scripts/tests.test.ts
@@ -0,0 +1,13 @@
+describe("CareMetX challenge tests", () => {
+  it("Verify the data in flat file matches the data in Patients collection.", () => {
+    throw new Error("Test Not Implemented");
+  });
+
+  it("Verify Emails were created in Emails Collection for patients who have CONSENT as Y.", () => {
+    throw new Error("Test Not Implemented");
+  });
+
+  it("Verify emails for each patient are scheduled correctly.", () => {
+    throw new Error("Test Not Implemented");
+  });
+});
