26f8e0db019380443426d6e12164cae9046bb3c8
Michael White <michaelwhite404@gmail.com>
2022-11-30 20:35:08 -0500

    Verify emails are from consenting patients

--- src/scripts/tests.test.ts
+++ src/scripts/tests.test.ts
@@ -1,13 +1,25 @@
+import { closeDB, openDB } from "../db";
+import { Email } from "../models";
+
+beforeAll(() => {
+  openDB();
+});
+
+afterAll(() => closeDB());
+
 describe("CareMetX challenge tests", () => {
   it("Verify the data in flat file matches the data in Patients collection.", () => {
     throw new Error("Test Not Implemented");
   });
 
-  it("Verify Emails were created in Emails Collection for patients who have CONSENT as Y.", () => {
-    throw new Error("Test Not Implemented");
+  it("Verify Emails were created in Emails Collection for patients who have CONSENT as Y.", async () => {
+    const emails = await Email.find({}).populate("patient", "consent");
+    // @ts-ignore
+    expect(emails.every((email) => email.patient.consent === "Y")).toBe(true);
   });
 
   it("Verify emails for each patient are scheduled correctly.", () => {
     throw new Error("Test Not Implemented");
   });
+  closeDB();
 });
