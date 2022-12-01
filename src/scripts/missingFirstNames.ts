3f1c49fb2c475c6a2e0ac3c44a05d3f48b3580ca
Michael White <michaelwhite404@gmail.com>
2022-11-30 19:01:00 -0500

    All Patient IDs where the first name is missing.

---
+++ src/scripts/missingFirstNames.ts
@@ -0,0 +1,17 @@
+import { closeDB, openDB } from "../db";
+import { Patient } from "../models";
+import pluralize from "pluralize";
+
+const missingFirstName = async () => {
+  openDB();
+  const patients = await Patient.find({ $or: [{ firstName: { $exists: 0 } }, { firstName: "" }] });
+  console.log(
+    `There are ${pluralize("patients", patients.length, true)} with a missing first name.`
+  );
+  if (patients.length > 0) {
+    console.log("Missing first name IDs:", patients.map((patient) => patient.memberId).join(", "));
+  }
+  closeDB();
+};
+
+missingFirstName().catch(console.log);
